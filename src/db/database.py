c79a37de5b5f1285c00063bc7883184cb93dc7be
loiz <Luis.Glaser@stud.uni-frankfurt.de>
2018-12-06 12:34:19 +0100

    🐿  half-baked db select interface, small changes to structure and arguments o loading

--- src/db/database.py
+++ src/db/database.py
@@ -17,11 +17,48 @@ class Database(object):
         """Use this to get raw connection to operate with"""
         return self.conn
 
-    def get_songs(self, title = "", others = []):
+    def select(self, query, criteria, fetchone = False):
+        """dumb recylable select statement sender, expects
+        a query with named (!) values plus a dict."""
+        cur = self.get_connection().cursor()
+        cur.execute(query, criteria)
+        if fetchone:
+            return cur.fetchone()
+        else:
+            return cur.fetchall()
+
+    def simple_query(self, columns = "*",
+                        title = False,
+                        artist = False,
+                        genre = False,
+                        # begin = False, TODO: implement
+                        # end = False, TODO: implement
+                        # generic_criteria = {}, TODO: implement
+                        fetchone = False):
         """ general query, can then be customed by named arguments
         on what criteria
         """
-        raise MethodNotImplementedError
+        criteria = {}
+        if title:
+            criteria["title"] = title
+        elif artist:
+            criteria["artist"] = artist
+        elif genre:
+            criteria["genre"] = genre
+        # elif begin:
+        #     criteria["begin"] = begin
+        # elif end:
+        #     criteria["end"] = end
+        statement = "SELECT "+",".join(columns)+" FROM songs WHERE "\
+                    + ",".join([("%s=:%s" %
+                            (key, key)) for key in criteria.keys()])\
+                    + ";"
+        print(statement)
+        return self.select(statement, criteria, fetchone)
+
+
+
+
 
 if __name__ == "__main__":
     # TODO: write bunch of tests
---
+++ src/load_to_db.py
@@ -0,0 +1,31 @@
+from src.db import database
+import csv
+import sys
+db = database.Database()
+connection = db.get_connection()
+# connection.execute("SELECT *;")
+BATCHSIZE = 1000
+with open(sys.argv[1], encoding='utf-8', mode='r') as f:
+    try:
+        from tqdm import tqdm
+        iterator = tqdm(csv.DictReader(f))
+    except ModuleNotFoundError:
+        iterator = csv.DictReader(f)
+    c = 0
+    try:
+        statements = []
+        for line in iterator:
+            c += 1
+            # [print(type(line[key])) for key in ['song','artist','year','genre','lyrics']]
+            statement = {key : line[key] for key in
+                        ['song','artist','year','genre','lyrics']}
+            statements.append(statement)
+            # connection.execute("INSERT INTO songs VALUES(NULL, :song, :artist, :year, :genre, :lyrics)", statement)
+
+            if c % BATCHSIZE == 0:
+                connection.executemany("INSERT INTO songs VALUES(NULL, :song, :artist, :year, :genre, :lyrics)", statements)
+                statements.clear()
+    except csv.Error:
+            print('csv choked on line %s' % (c+1))
+            connection.close()
+    connection.close()
