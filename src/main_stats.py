6b34f1e7086e07dd51e11109faa097bf2ea4a9dd
glaser <Luis.Glaser@stud.uni-frankfurt.de>
2019-03-01 12:27:53 +0100

    🐿  Added visualization script for histograms

--- src/main_stats.py
+++ src/main_stats.py
@@ -7,3 +7,11 @@
 
 from numerical_stats import visualize_data
 visualize_data.run("../data/img/spider.png")
+
+
+########################################
+# create histogram visualization
+########################################
+from numerical_stats import visualize_distribution
+visualize_distribution.kaggle_histogram("../data/lyrics.csv")
+visualize_distribution.genius_histogram()
